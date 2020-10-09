46a48b679940531f4b81953ab98d209b33573ddf
Vanessa Lane <vlane@23andme.com>
2020-10-08 23:22:20 -0700

    add FriendList, ReactionList, Profile, SingleThought

--- client/src/App.js
+++ client/src/App.js
@@ -14,27 +14,25 @@ import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 
-function App() {
-  const client = new ApolloClient({
-    uri: 'graphql'
-  });
+const client = new ApolloClient();
 
+function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
           <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/profile/:username?" component={Profile} />
-              <Route exact path="/thought/:id" component={SingleThought} />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/signup" component={Signup} />
+                <Route exact path="/profile/:username?" component={Profile} />
+                <Route exact path="/thought/:id" component={SingleThought} />
 
-              <Route component={NoMatch} />
-            </Switch>
-          </div>
+                <Route component={NoMatch} />
+              </Switch>
+            </div>
           <Footer />
         </div>
       </Router>
